695c1152b47510d28741f165040a7209376e191b
pauldropz <pauldropz@gmail.com>
2022-10-01 15:58:12 +0100

    feat:created paths

---
+++ src/Route.js
@@ -0,0 +1,14 @@
+export default Object.freeze({
+    home: '/',
+       analytics: "/dashboard/analytics",
+       fintech: "/dashboard/fintech",
+       customers: "/ecommerce/customers",
+       orders: "/ecommerce/orders" ,
+       invoice: "/ecommerce/invoices",
+       shop: "/ecommerce/shop",
+      shop2: "/ecommerce/shop-2",
+      product: "/ecommerce/product",
+      cart: "/ecommerce/cart",
+
+})
+
