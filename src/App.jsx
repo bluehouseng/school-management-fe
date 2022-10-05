cb5fe427d404a19e3fc598c2a4f02306493be432
Justice Chima <justicec566@gmail.com>
2022-10-05 13:08:59 +0100

    routing setup and format

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@ import React, { useEffect } from 'react';
 import {
   Routes,
   Route,
-  useLocation
+  useLocation,
 } from 'react-router-dom';
 
 import './css/style.scss';
 
 import './charts/ChartjsConfig';
-import pages from './pages';
+import PageRoutes, { general } from './pages';
 
 function App() {
+  const user = {
+    isAdmin: false,
+    role: 'student'
+  };
+
+  const renderRoute = {
+    student: PageRoutes.student.map((item, idx) => <Route key={idx} path={item.path} element={item.element} />),
+    hod: PageRoutes.hod.map((item, idx) => <Route key={idx} path={item.path} element={item.element} />),
+    registrar:PageRoutes.registrar.map((item, idx) => <Route key={idx} path={item.path} element={item.element} />),
+    isAdmin: PageRoutes.pages.map((item, idx) => <Route key={idx} path={item.path} element={item.element} />)
+  }
 
   const location = useLocation();
 
@@ -20,15 +31,30 @@ function App() {
     document.querySelector('html').style.scrollBehavior = ''
   }, [location.pathname]); // triggered on route change
 
+  const userRole = (user?.isAdmin && 'isAdmin') || user?.role;
+  const CommonPath = general().map((item) => <Route key={item.path} path={item.path} element={item.element}/>);
+  const LoggedOut = !user && PageRoutes.loggedOut.map((item, idx) =>  <Route key={idx} path={item.path} element={item.element} />)
+
   return (
     <>
       <Routes>
-        {
-          pages.map((item, idx) => <Route key={idx} path={item.path} element={item.element} />)
-        }
+        {/* <Route path='/' element={(() => (<h1>Hello</h1>))()}/> */}
+        {renderRoute[userRole] }
+        {LoggedOut}
+        {CommonPath}
       </Routes>
     </>
   );
 }
 
 export default App;
+
+        // {/* {
+        //   pages.map((item, idx) => <Route key={idx} path={item.path} element={item.element} />)
+        // } */}
+        // {/* {
+        //   user === 'student' ? student.map((item, idx) => <Route key={idx} path={item.path} element={item.element} />) : null
+        // }
+        // {
+        //   user === 'hod' ? hod.map((item, idx) => <Route key={idx} path={item.path} element={item.element} />) : null
+        // } */}
