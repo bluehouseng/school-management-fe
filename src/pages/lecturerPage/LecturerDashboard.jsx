3fd52fdebaf0bba09bb89de16883403650e2da4c
Justice Chima <justicec566@gmail.com>
2022-10-18 20:53:39 +0100

    worked on the bug and the lecturer dashboard

---
+++ src/pages/lecturerPage/LecturerDashboard.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+// import LecturersSidebar from './LecturersSidebar';
+// import Header from '../../partials/Header';
+// import StudentAnalytics from './StudentAnalytics';
+// import WelcomeBanner from '../../partials/dashboard/WelcomeBanner';
+// import DashboardAvatars from '../../partials/dashboard/DashboardAvatars';
+// import FilterButton from '../../components/DropdownFilter';
+// import Datepicker from '../../components/Datepicker';
+// import StudentCard01 from './StudentCard/StudentCard01';
+// import StudentCard02 from './StudentCard/StudentCard02';
+// import StudentCard03 from './StudentCard/StudentCard03';
+// import StudentCard04 from './StudentCard/StudentCard04';
+// import StudentCard05 from './StudentCard/StudentCard05';
+// import LecturersHeader from './LecturersHeader';
+// import DashboardCard06 from '../../partials/dashboard/DashboardCard06';
+// import DashboardCard07 from '../../partials/dashboard/DashboardCard07';cdcdcdcd
+// import DashboardCard08 from '../../partials/dashboard/DashboardCard08';
+// import DashboardCard09 from '../../partials/dashboard/DashboardCard09';
+// import DashboardCard10 from '../../partials/dashboard/DashboardCard10';
+// import DashboardCard11 from '../../partials/dashboard/DashboardCard11';
+
+function LecturerDashboard() {
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+   <h2>
+    love
+   </h2>
+  );
+}
+
+export default LecturerDashboard;
