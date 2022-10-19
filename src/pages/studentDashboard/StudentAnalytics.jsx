b33d49e8059c5be9de2571f63620e49cd3919609
Samagent97 <sammccarthy090@gmail.com>
2022-10-19 12:20:29 +0100

    will be ignored

--- src/pages/studentDashboard/StudentAnalytics.jsx
+++ src/pages/studentDashboard/StudentAnalytics.jsx
@@ -2,14 +2,8 @@ import React from 'react'
 
 const StudentAnalytics = () => {
   return (  
-<<<<<<< HEAD
-    <div className="flex flex-row flex-wrap justify-evenly px-10 py-10 rounded-lg ">
-        
-        <div className=" bg-slate-200 px-5 py-5 rounded-lg drop-shadow-xl ">
-=======
     <div className="flex flex-wrap justify-between rounded-lg ">
         <div className=" bg-slate-200 px-14 py-5  mt-3 rounded-lg drop-shadow-xl ">
->>>>>>> ea6f5b7a97040f29db1d7da8483d91ae639e0b22
              <p>Total No. of Courses</p>
             <p className='font-bold text-2x0.5'> 20</p>
         </div>
